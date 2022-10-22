ed33b2500cfba0aa5e8c9d44bf893a7fde5ac19a
Adeolu Gideon <gideonhardey@gmail.com>
2022-10-22 09:05:13 +0100

    first commit

---
+++ php/login.php
@@ -0,0 +1,18 @@
+<?php
+if(isset($_POST['submit'])){
+    $username = $_POST['username'];//finish this line
+    $password = $_POST['password'];//finish this
+
+loginUser($email, $password);
+
+}
+
+function loginUser($email, $password){
+    /*
+        Finish this function to check if username and password 
+    from file match that which is passed from the form
+    */
+}
+
+echo "HANDLE THIS PAGE";
+
